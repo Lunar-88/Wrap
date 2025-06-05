afec135f9e91723ae8490ed26566ef0fd6db4c1c
Felix Kemboi <felixkemboi824@gmail.com>
2025-06-05 09:09:12 +0300

    navbar done

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 
 function Navbar() {
   return (
-    <nav class="flex bg-">
-       <div>
-         <img src="logo.png" alt="logo" class="h-10 w-10" />
+    <nav class="flex bg-white w-full justify-between items-center px-10 py-10 shadow-md mx-auto">
+       <div class="pl-10">
+         <img src="logos.png" alt="logo" class="h-10 w-10 scale-[8] ml-10"/>
        </div>
-        <ul class="flex space-x-4">
-          <li><a href="#home" class="hover:text-blue-200">Home</a></li>
-          <li><a href="#about" class="hover:text-blue-200">About</a></li>
-          <li><a href="#services" class="hover:text-blue-200">Services</a></li>
-          <li><a href="#contact" class="hover:text-blue-200">Contact</a></li>
+       
+       <ul class="flex space-x-12 gap-x-14 font-semibold text-lg w-100 justify-center items-center ">
+          <li><a href="#home" class="hover:text-white hover:bg-black px-4 py-2 rounded-md">Home</a></li>
+          <li><a href="#about" class="hover:text-white hover:bg-black px-4 py-2 rounded-md">Services</a></li>
+          <li><a href="#services" class="hover:text-white hover:bg-black px-4 py-2 rounded-md">FAQs</a></li>
+          <li><a href="#contact" class="hover:text-white hover:bg-black px-4 py-2 rounded-md">Contact</a></li>
         </ul>
-        <div>
-          <button class="bg-white text-blue-500 px-4 py-2 rounded hover:bg-gray-200">Book Now</button>
+       
+        <div class="bg-black hover:bg-slate-700 text-white px-4 py-2 rounded-md ">
+          <button class="font-semibold ">Book Now</button>
         </div>
     </nav>
   );
