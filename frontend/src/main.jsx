31d431cf6bd78c73ae024b35f56c6827661adfb2
Felix Kemboi <felixkemboi824@gmail.com>
2025-08-18 23:49:04 +0300

    maps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom'; // ✅ Use HashRouter instead of BrowserRouter
 import App from './App.jsx';
 import './index.css'; // Tailwind CSS import
+import "leaflet/dist/leaflet.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
